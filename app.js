46954405a33b59425fc39b1b9e5eb41b0ecce95e
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-08-04 23:20:02 +0530

    feat: added hamburger button

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const hamburger = document.querySelector(".nav__hamburger");
 const navMenu = document.querySelector(".nav__menu");
 
-hamburger.addEventListener("click", mobileMenu);
 
-function mobileMenu() {
+const mobileMenu = () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-}
+};
 
+hamburger.addEventListener("click", mobileMenu);
 const navLink = document.querySelectorAll(".nav__link");
 
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
-function closeMenu() {
+const closeMenu = () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}
+};
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+// Content Hamburger
+
+const contentHamburger = document.querySelector(".content__hamburger");
+const contentMenu = document.querySelector(".content-left");
+
+
+const contentMobileView = () => {
+    contentHamburger.classList.toggle("active");
+    contentMenu.classList.toggle("active");
+};
+contentHamburger.addEventListener("click", contentMobileView);
+
+const contentLink = document.querySelectorAll(".content-left__link");
+
+contentLink.forEach((n) => n.addEventListener("click", contentCloseMenu));
+
+const contentCloseMenu = () => {
+    contentHamburger.classList.remove("active");
+    contentLink.classList.remove("active");
+};
